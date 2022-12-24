9bfce0d5a59d9a8a62bfd5a0ca29f69c15fd6c48
tfornal <tomasz.fornal6@gmail.com>
2022-12-24 15:28:53 +0100

    rotation slight improvement, needs to be adjusted to the rotation axis

--- src/_geometry/dispersive_element_OOP.py
+++ src/_geometry/dispersive_element_OOP.py
@@ -8,7 +8,6 @@ import json
 
 
 class DispersiveElement:
-    
     def __init__(self, element, crystal_height_step=10, crystal_length_step=20):
         """
         This class creates the curved surface of the selected dispersive element
@@ -24,7 +23,7 @@ class DispersiveElement:
             Accuracy of the crystals length mesh (by default 20 points).
         TODO test!!!!
         ALE CHUJNIA! POPRAWIC TODO
-        TODO - zmiana promienia krzywizny krysztalu na dowolna wartoć - zahardkodowanie pozycji krysztalu w konkretnej pozycji 
+        TODO - zmiana promienia krzywizny krysztalu na dowolna wartoć - zahardkodowanie pozycji krysztalu w konkretnej pozycji
         """
 
         self.height_step = crystal_height_step
@@ -44,12 +43,18 @@ class DispersiveElement:
             self.crystal_central_point, self.radius_central_point
         )
         self.crystal_orientation_vector = self.B - self.C
+
+        #############
+        self.srodek = (self.A + self.B) / 2  # - (self.C + self.D) / 2
+        ##########
+
         self.shifted_radius_central_point = self.define_radius_central_point()
 
         self.alpha = self.angle_between_lines(self.radius_central_point, self.A, self.B)
 
         # self.shift_angle = self.calculate_shift_angle()
         print(element, self.R)
+
     @staticmethod
     def get_coordinates(element):
         """_summary_
@@ -61,12 +66,12 @@ class DispersiveElement:
             _type_: _description_
         """
         with open(
-            pathlib.Path.cwd() / "coordinates.json"
+            pathlib.Path.cwd() / "src" / "_geometry" / "coordinates.json"
         ) as file:
             json_file = json.load(file)
             disp_elem_coord = json_file["dispersive element"]["element"][f"{element}"]
         return disp_elem_coord
-    
+
     @staticmethod
     def distance_between_poinst(p1, p2):
         """Accepts only np.array containing 3 int/float
@@ -98,7 +103,7 @@ class DispersiveElement:
         Return - 2d array with included vector.
         """
         coordinates_plus_vector = object_coordinates + crystal_orientation_vector
-        
+
         return coordinates_plus_vector
 
     def angle_between_lines(self, central_point, p1, p2):
@@ -114,7 +119,7 @@ class DispersiveElement:
         central_point, p1, p2 = Point3D(central_point), Point3D(p1), Point3D(p2)
         l1, l2 = Line3D(central_point, p1), Line3D(central_point, p2)
         angle = degrees(l1.angle_between(l2))
-        
+
         return angle
 
     def rotation_matrix_3D(self, theta, axis):
@@ -138,51 +143,18 @@ class DispersiveElement:
             ]
         )
 
-
-    # def calculate_shift_angle(self):
-    #     """TODO!!!!!!!!!!!!!!!!"""
-    #     crys_height = np.linspace(0, 20, self.height_step)
-    #     crys_lenght = np.linspace(0, self.alpha, num=self.length_step) / 180 * np.pi
-
-    #     X = self.R * np.cos(crys_lenght)
-    #     Y = self.R * np.sin(crys_lenght)
-
-    #     px = np.repeat(X, self.height_step)
-    #     py = np.repeat(Y, self.height_step)
-    #     pz = np.tile(crys_height, self.length_step)
-    #     points = np.vstack((pz, px, py)).T
-    #     # breakpoint()
-        
-    #     ovec = self.crystal_orientation_vector / (
-    #         np.linalg.norm(self.crystal_orientation_vector)
-    #     )
-    #     cylvec = np.array([1, 0, 0])
-
-    #     if np.allclose(cylvec, ovec):
-    #         return points
-
-    #     oaxis = np.cross(ovec, cylvec)
-    #     rot = np.arccos(np.dot(ovec, cylvec))
-
-    #     R = self.rotation_matrix_3D(rot, oaxis)
-    #     cylinder_points = points.dot(R)
-
-    #     shifted_cylinder_points = self.shift_cylinder(
-    #         cylinder_points, self.shifted_radius_central_point
-    #     )
-    #     cylinders_first_coordinate = shifted_cylinder_points[0]
-    #     shift_angle = self.angle_between_lines(
-    #         self.shifted_radius_central_point, self.A, cylinders_first_coordinate
-    #     )
-    #     print(float("{:.2f}".format(shift_angle)))
-    #     return float("{:.2f}".format(shift_angle))
-
     def make_curved_crystal(self):
-        """TODO!!!!!!!!!!!!!!!!"""
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         crys_height = np.linspace(0, 20, self.height_step)  # crystal height range
 
         crys_lenght = (
-            np.linspace(0, self.alpha,
+            np.linspace(
+                0,
+                self.alpha,
                 num=self.length_step,
             )
             / 180
@@ -192,18 +164,13 @@ class DispersiveElement:
         X = self.R * np.cos(crys_lenght)
         Y = self.R * np.sin(crys_lenght)
 
-        px = np.repeat(X, self.height_step )
-        py = np.repeat(Y, self.height_step )
+        px = np.repeat(X, self.height_step)
+        py = np.repeat(Y, self.height_step)
         pz = np.tile(crys_height, self.length_step)
         points = np.vstack((pz, px, py)).T
-        
-        ### ustawianie macierzy powierzchni zgodnie z orientacja osi krysztalu
-        shift = np.array(self.crystal_central_point) - np.mean(points, axis=0)
-        points += shift
-        
-        
-        
-        
+
+        ## ustawianie macierzy powierzchni zgodnie z orientacja osi krysztalu
+
         ovec = self.crystal_orientation_vector / (
             np.linalg.norm(self.crystal_orientation_vector)
         )
@@ -214,24 +181,26 @@ class DispersiveElement:
 
         oaxis = np.cross(ovec, cylvec)
         rot = np.arccos(np.dot(ovec, cylvec))
-        
+
+        # rot = 15
+        # oaxis = self.crystal_orientation_vector
+
         rot_matrix = self.rotation_matrix_3D(rot, oaxis)
-        cylinder_points = points.dot(rot_matrix)
+        points = points.dot(rot_matrix)
 
-        positioned_crystal_surface = cylinder_points + self.shifted_radius_central_point
+        shift = np.array(self.crystal_central_point) - np.mean(points, axis=0)
+        points += shift
 
-    
-        angle = self.angle_between_lines(self.shifted_radius_central_point, positioned_crystal_surface[-1], self.A)
-        # self.rotation_matrix_3D(positioned_crystal_surface, )
-        print(angle)
-        
-        
-        
+        ## kolejne przesuniecie
+
+        rot_matrix2 = self.rotation_matrix_3D(0.3, self.srodek)
+        points = points.dot(rot_matrix2)
+
         return points
 
 
 if __name__ == "__main__":
-    disp_elem = ["B", "C", "N", "O"]
+    disp_elem = ["B"]  # , "C", "N", "O"]
     fig = pv.Plotter()
     fig.set_background("black")
     for element in disp_elem:
@@ -240,40 +209,19 @@ if __name__ == "__main__":
 
         fig.add_mesh(np.array(disp.crystal_central_point), color="blue", point_size=10)
         fig.add_mesh(disp.A, color="red", point_size=20)
+        fig.add_mesh(disp.srodek, color="green", point_size=20)
         fig.add_mesh(disp.B, color="red", point_size=10)
         fig.add_mesh(disp.C, color="red", point_size=10)
         fig.add_mesh(disp.D, color="red", point_size=10)
         fig.add_mesh(crys, color="orange", render_points_as_spheres=True)
-        fig.add_mesh(crys[-1], color="red", render_points_as_spheres=True, point_size = 20)
+        fig.add_mesh(
+            crys[-1], color="red", render_points_as_spheres=True, point_size=20
+        )
 
     fig.show()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-###########BACKUP
 
+
+###########BACKUP
 
 
 # import numpy as np
@@ -286,7 +234,7 @@ if __name__ == "__main__":
 
 
 # class DispersiveElement:
-    
+
 #     def __init__(self, element, crystal_height_step=10, crystal_length_step=20):
 #         """
 #         This class creates the curved surface of the selected dispersive element
@@ -302,7 +250,7 @@ if __name__ == "__main__":
 #             Accuracy of the crystals length mesh (by default 20 points).
 #         TODO test!!!!
 #         ALE CHUJNIA! POPRAWIC TODO
-#         TODO - zmiana promienia krzywizny krysztalu na dowolna wartoć - zahardkodowanie pozycji krysztalu w konkretnej pozycji 
+#         TODO - zmiana promienia krzywizny krysztalu na dowolna wartoć - zahardkodowanie pozycji krysztalu w konkretnej pozycji
 #         """
 
 #         self.height_step = crystal_height_step
@@ -327,7 +275,7 @@ if __name__ == "__main__":
 #         self.alpha = self.angle_between_lines(self.radius_central_point, self.A, self.B)
 
 #         self.shift_angle = self.calculate_shift_angle()
-    
+
 #     @staticmethod
 #     def get_coordinates(element):
 #         """_summary_
@@ -344,7 +292,7 @@ if __name__ == "__main__":
 #             json_file = json.load(file)
 #             disp_elem_coord = json_file["dispersive element"]["element"][f"{element}"]
 #         return disp_elem_coord
-    
+
 #     @staticmethod
 #     def distance_between_poinst(p1, p2):
 #         """Accepts only np.array containing 3 int/float
@@ -376,7 +324,7 @@ if __name__ == "__main__":
 #         Return - 2d array with included vector.
 #         """
 #         coordinates_plus_vector = object_coordinates + crystal_orientation_vector
-        
+
 #         return coordinates_plus_vector
 
 #     def angle_between_lines(self, central_point, p1, p2):
@@ -392,7 +340,7 @@ if __name__ == "__main__":
 #         central_point, p1, p2 = Point3D(central_point), Point3D(p1), Point3D(p2)
 #         l1, l2 = Line3D(central_point, p1), Line3D(central_point, p2)
 #         angle = degrees(l1.angle_between(l2))
-        
+
 #         return angle
 
 #     def rotation_matrix_3D(self, theta, axis):
@@ -432,7 +380,7 @@ if __name__ == "__main__":
 #         pz = np.tile(crys_height, self.length_step)
 #         points = np.vstack((pz, px, py)).T
 #         breakpoint()
-        
+
 #         ovec = self.crystal_orientation_vector / (
 #             np.linalg.norm(self.crystal_orientation_vector)
 #         )
@@ -463,7 +411,7 @@ if __name__ == "__main__":
 #         """I tutaj zaczyna sie powtorzenie - trzeba to inaczej napisac!!!!"""
 #         I = np.linspace(0, 20, self.height_step)  # crystal height range
 
-#         """  POPRAWKA DLA KRYSZATLU BV - sprawdzic keidy sin(a) <0 a kiedy >0 - 
+#         """  POPRAWKA DLA KRYSZATLU BV - sprawdzic keidy sin(a) <0 a kiedy >0 -
 #         na tej podstawie okreslic czy shift angle ma wartosc + czy -"""
 #         """  POPRAWKA DLA KRYSZATLU OVIII "-0.3" STOPNIA ABY CYLINDER SIE DODATKOWO OBROCIL
 #         KONIECZNOSC WPROWADZENIA ZMIAN WE WSPOLRZEDNYCH PRAWDOPDOOBNIE PUNKTU OSI PROMIENIA KRYSZATLU"""
@@ -575,4 +523,3 @@ if __name__ == "__main__":
 #         fig.add_mesh(crys, color="orange", render_points_as_spheres=True)
 
 #     fig.show()
-
