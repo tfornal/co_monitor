4977ce25dc76fa51830b5bfe31dd7214230eb0bb
tfornal <tomasz.fornal6@gmail.com>
2023-02-05 18:34:18 +0100

    test corrected

--- tests/unit/test_rotation_matrix.py.py
+++ tests/unit/test_rotation_matrix.py.py
@@ -1,36 +1,38 @@
 import numpy as np
+import pytest
 
 
-def test_rotation_matrix(rot):
-    if rot.ndim != 2 or rot.shape[0] != rot.shape[1]:
-        return False
-    is_identity = np.allclose(rot.dot(rot.T), 
-                                     np.identity(rot.shape[0], float))
-    is_one = np.allclose(np.linalg.det(rot), 1)
-    
-    return is_identity and is_one 
+def test_rotation_matrix_90_degrees_around_x_axis():
+    theta = np.pi / 2
+    axis = [1, 0, 0]
+    rot_matrix = rotation_matrix(theta, axis)
+
+    expected_rot_matrix = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
+
+    np.testing.assert_allclose(rot_matrix, expected_rot_matrix)
 
 
-if __name__ == '__main__':
-    rot = np.array([
-        [0, 0, 1],
-        [1, 0, 0],
-        [0, 1, 0],
-    ])
-    print(test_rotation_matrix(rot))  # True
-    
-    
-    rot = np.array([
-        [-0.05055035,  0.06100042,  0.99685687],
-        [-0.06100042,  0.99608083, -0.06404625],
-        [-0.99685687, -0.06404625, -0.04663118]])
-    print(test_rotation_matrix(rot))  # True
-    
-    
-    rot = np.array([
-        [-1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-    ])
-    print(test_rotation_matrix(rot))  # False
-    print(test_rotation_matrix(np.zeros((3, 2))))  # False
+def test_rotation_matrix_180_degrees_around_y_axis():
+    theta = np.pi
+    axis = [0, 1, 0]
+    rot_matrix = rotation_matrix(theta, axis)
+
+    expected_rot_matrix = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
+
+    np.testing.assert_allclose(rot_matrix, expected_rot_matrix)
+
+
+def test_rotation_matrix_45_degrees_around_z_axis():
+    theta = np.pi / 4
+    axis = [0, 0, 1]
+    rot_matrix = rotation_matrix(theta, axis)
+
+    expected_rot_matrix = np.array(
+        [
+            [np.sqrt(2) / 2, -np.sqrt(2) / 2, 0],
+            [np.sqrt(2) / 2, np.sqrt(2) / 2, 0],
+            [0, 0, 1],
+        ]
+    )
+
+    np.testing.assert_allclose(rot_matrix, expected_rot_matrix)
