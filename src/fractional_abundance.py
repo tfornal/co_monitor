a39fc6db7b591a28eef8a7d60455eb00fc923edd
tfornal <tomasz.fornal6@gmail.com>
2023-02-12 20:10:28 +0100

    clean up, docstrings included

--- src/fractional_abundance.py
+++ src/fractional_abundance.py
@@ -1,46 +1,48 @@
 __author__ = "T. Fornal"
 __email__ = "tomasz.fornal6@gmail.com"
 
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-import pandas as pd
-from scipy import interpolate
 from pathlib import Path
 
-# TODO dolaczyc fractional abundance ze strahla
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import interpolate
 
 
 class FractionalAbundance:
-    """
-    The script reads the files representing fractional abundances (FA) of a given
+    """The script reads the files representing fractional abundances (FA) of a given
     ion and interpolates the data.
-    It is also possible to plot fractional abundances of selected ions.
-
-    Parameters:
-        element (str): select atoms symbol called in parentheses e.g. "C"
-        ion_state (str): atomic number of selected ion called in parentheses e.g. "Z5"
-        step (int): step of the interpolation - nominally 1 [eV] (optional)
     """
 
-    def __init__(self, element, ion_state, step=1, plot=False):
+    def __init__(self, element, ionization_state, interpolation_step=1, plot=False):
+        """
+        Parameters
+        ----------
+        element : str
+            Select atoms symbol called in parentheses e.g. "C"
+        ionization_state : str
+            Atomic number of selected ion called in parentheses e.g. "Z5"
+        interpolation_step : int, optional
+            Step of the interpolation - nominally 1 [eV] (optional), by default 1
+        plot : bool, optional
+            Plots retrieved Fractional Abundance, by default False
+        """
         self.element = element
-        self.ion_state = ion_state
-        self.step = step
+        self.ionization_state = ionization_state
+        self.interpolation_step = interpolation_step
         self.plot = plot
-        self.loaded_file_df = self.read_file()
-        self.df_interpolated_frac_ab = self.interpolated_fractional_abundance()
+        self.loaded_file_df = self._read_file()
+        self.df_interpolated_frac_ab = self._interpolate()
         if plot:
-            self.plotter()
+            self._plotter()
 
-    def read_file(self):
+    def _read_file(self) -> pd.DataFrame:
         """
         Reads the file representing fractional abundance of the given element.
 
-        Returns:
-            temperature (float list): temperature values [eV]
-            fractional_abundance (float list): fractional values of selected ion
-
+        Returns
+        -------
+        loaded_file_df : pandas.DataFrame
+            The loaded file data as a pandas dataframe.
         """
         fractional_abundance = (
             Path(__file__).parent.resolve()
@@ -49,23 +51,24 @@ class FractionalAbundance:
             / f"fractional_abundance_{self.element}.dat"
         )
         Path(__file__).parent.resolve()
-
         loaded_file_df = pd.read_csv(fractional_abundance, delimiter=" ")
 
         return loaded_file_df
 
-    def interpolated_fractional_abundance(self):
+    def _interpolate(self) -> np.ndarray:
         """
-        Interpolate the temperature and fractional abundance with given step.
+        Interpolates the temperature and fractional abundance with the given step.
 
-        Returns:
-            fractional_abundance_array wiextensions.ignoreRecommendations 2 columns: 1st - electron temeprature [eV],
-            2nd - fractional abundance [%]
+        Returns
+        -------
+        df_interpolated_frac_ab : np.ndarray
+            The fractional abundance dataframe with two columns:
+            1st - electron temperature [eV], 2nd - fractional abundance [%].
         """
 
         loaded_file_df = self.loaded_file_df
         temp_col = loaded_file_df["T"]
-        Te = np.arange(temp_col.iloc[0], temp_col.iloc[-1], self.step)
+        Te = np.arange(temp_col.iloc[0], temp_col.iloc[-1], self.interpolation_step)
         list_of_interp_ionisation_states = []
 
         # interpolate over all ionisation states and saves in a list
@@ -86,15 +89,21 @@ class FractionalAbundance:
 
         return df_interpolated_frac_ab
 
-    def save_to_file(self, element, ion_state):
-        """
-        Saves inteprolated temperautre/fractional_abundance to the file
+    def _save_to_file(self, element, ion_state):
+        """Saves inteprolated temperautre/fractional_abundance to the file.
+
+        Parameters
+        ----------
+        element : str
+            Symbol for the selected atom - "B", "C", "N" and "O".
+        ion_state : str
+            Atomic number of selected hydrogen like ion, for example "Z5" when considering "C" line.
         """
         df_interpolated_frac_ab = self.interpolated_fractional_abundance()
         np.savetxt(f"{element} {ion_state}.txt", df_interpolated_frac_ab)
         print(f"Fractional abundance of {element}-{ion_state} ion successfuly saved!")
 
-    def plotter(self):
+    def _plotter(self):
         """
         Plots interpolated T_e [eV] and all fractional abundance [%].
         """
@@ -108,11 +117,11 @@ class FractionalAbundance:
         plt.xlim(0, 220)
         plt.xlabel("Te [eV]")
         plt.ylabel("Fractional Abundance [%]")
-        plt.title(f"Fractional abundance of {self.element} ({self.ion_state})")
+        plt.title(f"Fractional abundance of {self.element} ({self.ionization_state})")
         plt.tight_layout()
 
         plt.show()
 
 
 if __name__ == "__main__":
-    fa = FractionalAbundance("O", "Z7", plot=False)
+    fa = FractionalAbundance("O", "Z7", plot=True)
