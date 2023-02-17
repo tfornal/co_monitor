ee9d98f14cf4c9a98e97c2df38c3bdc5898aecbe
tfornal <tomasz.fornal6@gmail.com>
2023-02-17 21:15:27 +0100

    refactoring finished

--- src/_geometry/reff_calculation.py
+++ src/_geometry/reff_calculation.py
@@ -1,14 +1,17 @@
 __author__ = "T. Fornal"
 __email__ = "tomasz.fornal6@gmail.com"
 
+from pathlib import Path
+from typing import List
+
 import aiohttp
 import asyncio
 import nest_asyncio
 import numpy as np
-from tqdm import tqdm
-from pathlib import Path
-from mesh_calculation import CuboidMesh
 import pandas as pd
+from tqdm import tqdm
+
+from mesh_calculation import CuboidMesh
 
 
 class ReffVMEC:
@@ -17,20 +20,25 @@ class ReffVMEC:
     def __init__(
         self,
         distance_between_points: int = 10,
-        cuboid_size: list = [1350, 800, 250],
+        cuboid_size: List[int] = [1350, 800, 250],
         chunk_size: int = 10000,
         save: bool = True,
     ):
-        """Odczytywanie wspolrzedne xyz uzyskanych przez plik "mesh_calculation.py"
-        oraz obliczanie przez kod VMEC reff i zpaisywanie do pliku.
-        Uruchamiane tylko na serwerach greifswaldowskich. Pobiera bezposrednio
-        tablice z funkcji "mesh_calculation" i wykonuje obliczenia.
-
-        Args:
-            distance_between_points (int): _description_. Defaults to 250.
-            cuboid_size (np.array): _description_. Defaults to [2000, 2000, 2000].
-            savetxt (bool): _description_. Defaults to False.
         """
+        Class to retrieve values of "reff" for a set of Cartesian coordinates using the VMEC code.
+
+        Parameters
+        ----------
+        distance_between_points : int, optional
+            Distance between points of the mesh used to define the plasma domain, by default 10.
+        cuboid_size : List[int], optional
+            Size of the cuboid containing the plasma domain in millimeters, by default [1350, 800, 250].
+        chunk_size : int, optional
+            Number of points per chunk, by default 10000.
+        save : bool, optional
+            If True, saves the result to a file, by default True.
+        """
+
         self.distance_between_points = distance_between_points
         self.cuboid_size = cuboid_size
         self.chunk_size = chunk_size
@@ -43,13 +51,27 @@ class ReffVMEC:
         if save:
             self._save_to_file()
 
-    def _get_plasma_coordinates(self):
+    def _get_plasma_coordinates(self) -> np.ndarray:
+        """
+        Generates an array of cartesian coordinates for a cuboid mesh.
+
+        Returns:
+            numpy.ndarray: An array of cartesian coordinates for a cuboid mesh.
+        """
         cm = CuboidMesh(self.distance_between_points, self.cuboid_size)
         cuboid_mesh = cm.outer_cube_mesh
 
         return cuboid_mesh
 
-    def _calc_chunks_nr(self):
+    def _calc_chunks_nr(self) -> int:
+        """
+        Calculate the number of chunks required for parallel API requests.
+
+        Returns:
+        -------
+        int
+        The number of chunks required for parallel API requests.
+        """
         if len(self.cuboid_coordinates) > self.chunk_size:
             chunks_number = len(self.cuboid_coordinates) // self.chunk_size + 1
         else:
@@ -62,14 +84,31 @@ class ReffVMEC:
 
     def _retrieve_from_API(self):
         """
-        Reads values of Reff corresponding to the cartesian coordinates. VMEC
-        accepts coordinates in meters.
+        Retrieves values of Reff corresponding to the cartesian coordinates from a VMEC API.
+
+        Returns
+        -------
+        np.ndarray
+            Array of Reff values for each plasma point in the cartesian coordinates.
         """
 
         reff = []
         for chunk in tqdm(range(self.chunks_nr)):
 
-            def get_tasks(session):
+            def get_tasks(session) -> list:
+                """
+                Generates HTTP GET requests for the plasma points in the current chunk.
+
+                Parameters
+                ----------
+                session : aiohttp.ClientSession
+                    Session object used to create the GET requests.
+
+                Returns
+                -------
+                List
+                    List of GET requests for the plasma points in the current chunk.
+                """
                 tasks = []
                 print(self.cuboid_coordinates)
                 for plas_point in self.cuboid_coordinates[
@@ -80,6 +119,9 @@ class ReffVMEC:
                 return tasks
 
             async def get_reff_points():
+                """
+                Sends HTTP GET requests to the VMEC API to retrieve Reff values for all plasma points.
+                """
                 async with aiohttp.ClientSession() as session:
 
                     tasks = get_tasks(session)
@@ -94,18 +136,38 @@ class ReffVMEC:
 
         return np.array(reff)
 
-    def create_df(self):
-        rounded_cuboid_coordinates = self.cuboid_coordinates.round(4)
-        df = pd.DataFrame()
-        df["idx"] = np.arange(len(self.cuboid_coordinates))
-        df["x [m]"] = rounded_cuboid_coordinates[:, 0]
-        df["y [m]"] = rounded_cuboid_coordinates[:, 1]
-        df["z [m]"] = rounded_cuboid_coordinates[:, 2]
-        df["Reff [m]"] = self.reff_array.astype(float).round(3)
+    def create_df(self) -> pd.DataFrame:
+        """Create a pandas DataFrame with rounded cuboid coordinates and Reff values.
+
+        Returns
+        -------
+        pd.DataFrame
+            A pandas DataFrame with the following columns:
+            - "idx": An index of length len(cuboid_coordinates).
+            - "x [m]": Rounded cuboid coordinates along the x-axis.
+            - "y [m]": Rounded cuboid coordinates along the y-axis.
+            - "z [m]": Rounded cuboid coordinates along the z-axis.
+            - "Reff [m]": Rounded values of self.reff_array.
+        """
+        # Round the cuboid coordinates to 4 decimal places.
+        rounded_cuboid_coordinates = np.round(self.cuboid_coordinates, decimals=4)
+
+        # Create a DataFrame with the rounded coordinates and Reff values.
+        df = pd.DataFrame(
+            {
+                "idx": np.arange(len(self.cuboid_coordinates)),
+                "x [m]": rounded_cuboid_coordinates[:, 0],
+                "y [m]": rounded_cuboid_coordinates[:, 1],
+                "z [m]": rounded_cuboid_coordinates[:, 2],
+                "Reff [m]": np.round(self.reff_array.astype(float), decimals=3),
+            }
+        )
 
         return df
 
     def _save_to_file(self):
+        """Save the Reff DataFrame to a file."""
+
         path = Path(__file__).parent.parent.resolve() / "_Input_files" / "Reff"
         self.reff_df.to_csv(
             path / f"Reff_coordinates-{self.distance_between_points}_mm.dat",
