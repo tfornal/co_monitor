abe037714123bef844a9a04a5b18c743259c1e77
tfornal <tomasz.fornal6@gmail.com>
2023-03-06 19:45:06 +0100

    main file finished

--- co_monitor/main.py
+++ co_monitor/main.py
@@ -16,9 +16,10 @@ import pandas as pd
 
 from emissivity.reader import Emissivity
 from emissivity.kinetic_profiles import TwoGaussSumProfile, ExperimentalProfile
+from geometry.simulation import Simulation
 
 
-def main():
+def main_emissivity(element, plot=False):
     elements = ["C", "B", "O", "N"]
     Element = namedtuple("Element", "ion_state wavelength impurity_fraction")
     lyman_alpha_line = {
@@ -36,43 +37,39 @@ def main():
     kinetic_profiles = TwoGaussSumProfile(n_e, T_e).profiles_df
     # kinetic_profiles = ExperimentalProfile("report_20181011_012@5_5000_v_1").profile_df
 
-    for element in elements:
-        line = lyman_alpha_line[element]
-        ce = Emissivity(
-            element,
-            line.ion_state,
-            line.wavelength,
-            line.impurity_fraction,
-            transitions,
-            reff_magnetic_config,
-            kinetic_profiles,
-        )
+    # for element in elements:
+    line = lyman_alpha_line[element]
+    ce = Emissivity(
+        element,
+        line.ion_state,
+        line.wavelength,
+        line.impurity_fraction,
+        transitions,
+        reff_magnetic_config,
+        kinetic_profiles,
+    )
+    if plot:
+        ce.plot()
+
+
+def main_geometry(element):
+    """Executes the simulation code in order to calculate
+    plasma volume observed by each spectroscopic channel.
+    Requires to input list of elements (B, C, N or O)"""
+
+    settings = dict(
+        slits_number=10,
+        distance_between_points=40,  # the lower value, the higher mesh precision - the longer computation
+        crystal_height_step=35,
+        crystal_length_step=30,
+        savetxt=False,
+        plot=True,
+    )
+    simul = Simulation(element, **settings)
 
 
 if __name__ == "__main__":
-    main()
-
-
-# def geometry():
-#     """Executes the simulation code in order to calculate
-#     plasma volume observed by each spectroscopic channel.
-#     Requires to input list of elements (B, C, N or O)"""
-
-#     from simulation import Simulation
-
-#     elements_list = ["C"]  # , "B" , "N", "O"]
-#     settings = dict(
-#         slits_number=10,
-#         distance_between_points=20,  # the lower value, the higher mesh precision - the longer computation
-#         crystal_height_step=15,
-#         crystal_length_step=10,
-#         savetxt=False,
-#         plot=True,
-#     )
-
-
-# if __name__ == "__main__":
-#     geometry()
-#     for element in elements_list:
-#         geom = geometry()
-#         simul = Simulation(element, **settings)
+    elements_list = ["B", "C", "N", "O"]
+    for element in elements_list:
+        # geom = main_geometry(element)
+        emiss = main_emissivity(element, plot=True)
