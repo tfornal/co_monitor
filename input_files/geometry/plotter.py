0cd4640042510e606caa994adf5dcf9c398c37e4
tfornal <tomasz.fornal6@gmail.com>
2023-03-08 15:23:03 +0100

    plotter corrected

--- input_files/geometry/plotter.py
+++ input_files/geometry/plotter.py
@@ -5,10 +5,11 @@ import pyvista as pv
 import pathlib
 
 
-def plotter(element):
+def get_data(element):
     cwd = pathlib.Path(__file__).parent.resolve()
     path = (
         cwd
+        / "observed_plasma_volume"
         / element
         / f"{element}_plasma_coordinates-10_mm_spacing-height_30-length_20-slit_100.dat"
     )
@@ -21,6 +22,11 @@ def plotter(element):
         "plasma_z",
         "total_intensity_fraction",
     ]
+    return df
+
+
+def plotter(element):
+    df = get_data(element)
     """Plots observed plasma volume including distribution of radiation intensity regions."""
     fig = pv.Plotter()
     fig.set_background("black")
