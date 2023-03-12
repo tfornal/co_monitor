aece0cdef7c16d57fd7e1460ef049773c06d90fd
tfornal <tomasz.fornal6@gmail.com>
2023-03-12 18:57:41 +0100

    test included

---
+++ tests/unit/emissivity/test_emissivity.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+from co_monitor.emissivity.reader import Emissivity
+
+
+list_of_nrs = [1, 22, 50, 16, 7, 19, 100, 4, 6]
+
+
+def test_find_nearest():
+    index = Emissivity._find_nearest(list_of_nrs, 75)
+    expected = 2
+    assert index == expected
+
+    index = Emissivity._find_nearest(list_of_nrs, 76)
+    expected = 6
+    assert index == expected
