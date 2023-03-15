9321da88eaf0f5c14e1846dc07fd82a320507ceb
tfornal <tomasz.fornal6@gmail.com>
2023-03-15 20:51:41 +0100

    testing approach

--- tests/unit/emissivity/test_emissivity.py
+++ tests/unit/emissivity/test_emissivity.py
@@ -4,7 +4,7 @@ import pytest
 from co_monitor.emissivity.reader import Emissivity
 
 
-list_of_nrs = [1, 22, 50, 16, 7, 19, 100, 4, 6]
+list_of_nrs = [1, 22, 50, 16, 7, 19, 100, 4, 6, 211]
 
 
 def test_find_nearest():
@@ -14,4 +14,8 @@ def test_find_nearest():
 
     index = Emissivity._find_nearest(list_of_nrs, 76)
     expected = 6
+    assert index == expected
+
+    index = Emissivity._find_nearest(list_of_nrs, 0)
+    expected = 0
     assert index == expected
